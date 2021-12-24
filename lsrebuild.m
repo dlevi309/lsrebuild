f230fd359bae5d39c54272ed4a9302a016bfc657
daniel <dlevi309@gmail.com>
2021-12-24 00:57:58 -0800

    cleanup + add more LS related entitlements (although unnecessary)

--- lsrebuild.m
+++ lsrebuild.m
@@ -19,36 +19,21 @@
 @interface LSApplicationWorkspace : NSObject
 + (LSApplicationWorkspace*)defaultWorkspace;
 - (NSArray*)applicationsAvailableForHandlingURLScheme:(NSString*)scheme;
-- (LSOpenOperation*)operationToOpenResource:(NSURL*)URL
-						   usingApplication:(NSString*)identifier
-				   uniqueDocumentIdentifier:(NSString*)document
-								   userInfo:(NSDictionary*)userInfo
-								   delegate:(id)delegate;
+- (LSOpenOperation*)operationToOpenResource:(NSURL*)URL usingApplication:(NSString*)identifier uniqueDocumentIdentifier:(NSString*)document userInfo:(NSDictionary*)userInfo delegate:(id)delegate;
 @end
 
 @interface LSApplicationWorkspace (LSPrivate)
-- (BOOL)_LSPrivateRebuildApplicationDatabasesForSystemApps:(BOOL)arg1
-												  internal:(BOOL)arg2
-													  user:(BOOL)arg3;
+- (BOOL)_LSPrivateRebuildApplicationDatabasesForSystemApps:(BOOL)arg1 internal:(BOOL)arg2 user:(BOOL)arg3;
 @end
 
 int main(int argc, char** argv) {
 	@autoreleasepool {
-		LSApplicationWorkspace* workspace =
-			[LSApplicationWorkspace defaultWorkspace];
-		if (![workspace
-				respondsToSelector:@selector
-				(_LSPrivateRebuildApplicationDatabasesForSystemApps:
-														   internal:user:)]) {
-			fputs("Not supported\n", stderr);
-			return -1;
+		LSApplicationWorkspace* workspace = [LSApplicationWorkspace defaultWorkspace];
+		if (![workspace respondsToSelector:@selector(_LSPrivateRebuildApplicationDatabasesForSystemApps:internal:user:)]) {
+			fputs("Incompatible\n", stderr);
 		}
-		if (![workspace
-				_LSPrivateRebuildApplicationDatabasesForSystemApps:YES
-														  internal:YES
-															  user:YES]) {
-			fputs("RebuildApplicationDatabases\n", stderr);
-			return 1;
+		if (![workspace _LSPrivateRebuildApplicationDatabasesForSystemApps:YES internal:YES user:YES]) {
+			fputs("Failed\n", stderr);
 		}
 		return 0;
 	}
